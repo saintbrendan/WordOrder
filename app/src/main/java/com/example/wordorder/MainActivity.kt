441b2d24b1ce900f80b51151dd008777cf54521e
Brendan Cunnie <saintbrendan@gmail.com>
2021-08-17 19:58:58 -0400

    Add sentence values.  Add button.

--- app/src/main/java/com/example/wordorder/MainActivity.kt
+++ app/src/main/java/com/example/wordorder/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.wordorder
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wordorder.adapter.WordsAdapter
@@ -11,7 +13,7 @@ import com.example.wordorder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     // values of the draggable views (usually this should come from a data source)
-    private val words = mutableListOf("Did", "you", "run", "the", "run", "right", "now", "?")
+    ////private val words = mutableListOf("Did", "you", "run", "the", "run", "right", "now", "?")
     // last selected word
     private var selectedWord = ""
 
@@ -21,6 +23,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        val wordArray4 = resources.getStringArray(R.array.sentence4)
+        val words = mutableListOf<String>(*wordArray4)
+
         val wordsAdapter = WordsAdapter {
             selectedWord = it
         }.apply {
@@ -28,17 +33,30 @@ class MainActivity : AppCompatActivity() {
         }
         val sentenceAdapter = SentenceAdapter(wordsAdapter)
 
-        binding.rvSentence.layoutManager = FlexboxLayoutManager(this, FlexDirection.ROW, FlexWrap.WRAP).apply {
-            justifyContent = JustifyContent.FLEX_START
-            alignItems = AlignItems.CENTER
-        }
+        binding.rvSentence.layoutManager =
+            FlexboxLayoutManager(this, FlexDirection.ROW, FlexWrap.WRAP).apply {
+                justifyContent = JustifyContent.FLEX_START
+                alignItems = AlignItems.CENTER
+            }
         binding.rvSentence.adapter = sentenceAdapter
 
-        binding.rvWords.layoutManager = FlexboxLayoutManager(this, FlexDirection.ROW, FlexWrap.WRAP).apply {
-            justifyContent = JustifyContent.FLEX_START
-            alignItems = AlignItems.CENTER
-        }
+        binding.rvWords.layoutManager =
+            FlexboxLayoutManager(this, FlexDirection.ROW, FlexWrap.WRAP).apply {
+                justifyContent = JustifyContent.FLEX_START
+                alignItems = AlignItems.CENTER
+            }
 
         binding.rvWords.adapter = wordsAdapter
+
+        val buttonID = R.id.button2
+        val button = findViewById<Button>(buttonID)
+        button.setOnClickListener { view ->
+            if (sentenceAdapter.currentList == words) {
+                button.setBackgroundColor(Color.GREEN)
+            } else {
+                button.setBackgroundColor(Color.RED)
+            }
+        }
+
     }
 }
